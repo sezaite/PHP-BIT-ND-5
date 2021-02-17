45b2d4273da2a83b25ed10c72ab7cf68a831ece7
sezaite <martyna.sezaite@gmail.com>
2021-02-17 15:12:18 +0000

    sesta

--- index.php
+++ index.php
@@ -148,4 +148,37 @@ usort($raidziuMasyvas, function($a, $b){
     return count($a) <=> count($b);
 });
 
-print_r($raidziuMasyvas);
+// print_r($raidziuMasyvas); <----------------------------------------------------------
+
+echo '<br><h1> PENKTAS . . . </h1><br>';
+
+// Sukurkite masyvą iš 30 elementų. Kiekvienas masyvo elementas yra masyvas [user_id => xxx, place_in_row => xxx] user_id atsitiktinis unikalus skaičius nuo 1 iki 1000000, place_in_row atsitiktinis skaičius nuo 0 iki 100. 
+
+for($i = 0; $i < 30; $i++){
+    $idMasyvas[$i] = ['user_id' => rand(1, 1000000), 'place_in_row' => rand(0, 100)];    
+}
+// print_r($idMasyvas); <--------------------------------------------------------
+
+echo '<br><h1> . . .SHESHI . . . </h1><br>';
+// Išrūšiuokite 5 uždavinio masyvą pagal user_id didėjančia tvarka. Ir paskui išrūšiuokite pagal place_in_row mažėjančia tvarka.
+
+usort($idMasyvas, function($a, $b){
+    return $a['user_id'] <=> $b['user_id'];
+});
+
+// print_r($idMasyvas); <----------------------------------------------------------------------
+
+usort($idMasyvas, function($a, $b){
+    return $b['place_in_row'] <=> $a['place_in_row'];
+});
+
+// print_r($idMasyvas); <-----------------------------------------------------------------------
+
+echo '<br><h1> . . .SiapTYNI . . . </h1><br>';
+// Prie 6 uždavinio masyvo antro lygio masyvų pridėkite dar du elementus: name ir surname. Elementus užpildykite stringais iš atsitiktinai sugeneruotų lotyniškų raidžių, kurių ilgiai nuo 5 iki 15.
+
+foreach($masyvas as $key => &$useris){
+    foreach($useris as $keyUs => $value){
+       
+    }
+}
